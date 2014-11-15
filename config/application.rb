ac05b4a121af239a4c7e64c4a7c8ae110fc00734
sai29 <saiprashanth93@gmail.com>
2014-11-15 11:08:06 +0530

    Changed application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.autoload_paths << Rails.root.join('lib')
 
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,6 +10,8 @@ Bundler.require(*Rails.groups)
 
 module Dailycheck
   class Application < Rails::Application
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
