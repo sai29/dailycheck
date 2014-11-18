2fc51b81fd94ac33e79b44b1048720f219296d6a
sai29 <saiprashanth93@gmail.com>
2014-11-18 22:35:05 +0530

    Wrote expression to only get email

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -5,10 +5,8 @@ class EmailProcessor
   end
 
   def process
-  	body =  @email.body
-  	puts body.class
-  	@team_member = TeamMember.find_by(email: @email.from)	
-  	answer = @email.body
+  	from =  @email.from.split.last.gsub((/[<>]/,''))
+  	@team_member = TeamMember.find_by(email: from)	
   	answer_form = @team_member.answer.build
   	answer_form.name = @team_member.name
     answer_form.answer = answer
