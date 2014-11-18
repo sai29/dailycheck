b73cc0e2d15108eb80d6c96026dc0efe1c60d8ad
sai29 <saiprashanth93@gmail.com>
2014-11-18 20:33:55 +0530

    Change email processor with build

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -7,7 +7,7 @@ class EmailProcessor
   def process
   	@team_member = current_user.team_members.find_by(email: @email)	
   	answer = @email.body
-  	post_form = @team_member.anwsers.new
+  	answer_form = @team_member.answer.build
   	answer_form.name = @team_member.name
     answer_form.answer = answer
     answer_form.save
