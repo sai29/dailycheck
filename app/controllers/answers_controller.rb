7122297bbc709d1e57fe30d6a938938f2729b934
sai29 <saiprashanth93@gmail.com>
2014-11-18 21:12:47 +0530

    Added authentication to answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-
+	before_action :authenticate_user!
 	def index
 		@answers = current_user.team_members.each do |team_member|
 			team_member.answer
