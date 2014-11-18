e58ba9c9e5390416894f114e5128b14d546dad76
sai29 <saiprashanth93@gmail.com>
2014-11-18 20:53:44 +0530

    Added griddler config

---
+++ config/initializers/griddler.rb
@@ -0,0 +1,7 @@
+require 'email_processor'
+Griddler.configure do |config|
+  config.processor_class = EmailProcessor
+  config.processor_method = :process
+  config.reply_delimiter = '-- REPLY ABOVE THIS LINE --'
+  config.email_service = :sendgrid
+end
