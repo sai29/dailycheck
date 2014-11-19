3bf012ed0a365443364eb2c18c35256f4bca512a
sai29 <saiprashanth93@gmail.com>
2014-11-19 14:39:15 +0530

    Removed a few mistakes

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -6,25 +6,18 @@ class EmailProcessor
 
   def process
     p @email.from[:email]
+    p @email.body
+    p @email.body.class
   	from1 =  @email.from.to_a.last.last.split.last.gsub(/[<>]/, '')
   	@team_member = TeamMember.find_by(email: @email.from[:email])
     p @team_member	
     p @team_member.inspect
-    p @team_member.i
+
   	answer_form = @team_member.answers.build
   	answer_form.name = @team_member.name
-    answer_form.answer = answer
+    answer_form.answer = @email.body
     answer_form.save
     puts "Saved"
   end
 
 end
-
-
-
-
-
-
-
-
-
