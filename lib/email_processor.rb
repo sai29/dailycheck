a99b2807ba62680949c7311f96f777300a82e0ce
sai29 <saiprashanth93@gmail.com>
2014-11-19 13:58:33 +0530

    Changed puts to p for debugging

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -6,10 +6,12 @@ class EmailProcessor
 
   def process
   	from1 =  @email.from.to_a.last.last.split.last.gsub(/[<>]/, '')
-    puts from1
-    puts from1.class
-  	@team_member = TeamMember.find_by(email: from1)	
-    puts @team_member
+    p from1
+    p from1.inspect
+    p from1.class
+  	@team_member = TeamMember.find_by(email: from1)
+    p @team_member	
+    p @team_member.inspect
   	answer_form = @team_member.answer.build
   	answer_form.name = @team_member.name
     answer_form.answer = answer
